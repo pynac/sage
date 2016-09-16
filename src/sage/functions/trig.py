64424a2f99df1b8fe4d021b455a8f1139c05e7cc
Ralf Stephan <ralf@ark.in-berlin.de>
2016-09-16 08:32:10 +0200

    cot tests

--- src/sage/functions/trig.py
+++ src/sage/functions/trig.py
@@ -257,6 +257,15 @@ class Function_cot(GinacFunction):
 
         TESTS:
 
+            sage: cot(float(0))
+            Infinity
+            sage: cot(SR(0))
+            Infinity
+            sage: cot(float(0.1))
+            9.966644423259238
+            sage: type(_)
+            <type 'float'>
+
         Test complex input::
 
             sage: cot(complex(1,1))     # rel tol 1e-15
